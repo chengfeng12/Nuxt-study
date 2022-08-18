cf2adda8aa64887cf5a8c4e6014a7ee96031856e
YXR <913457727@qq.com>
2022-08-18 10:37:20 +0800

    feat: init

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    首页
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      title: '这是一个标题'
+    }
+  },
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'My custom description'
+        }
+      ]
+    }
+  }
+}
+</script>
